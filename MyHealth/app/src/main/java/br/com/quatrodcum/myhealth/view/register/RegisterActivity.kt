4ce603c81e297dc2bb846d6a11a82edd1cc9abd8
FranciellyDiasM <frandiasma@gmail.com>
2024-09-22 22:50:50 -0300

    desenho da tela de cadastro

--- MyHealth/app/src/main/java/br/com/quatrodcum/myhealth/view/register/RegisterActivity.kt
+++ MyHealth/app/src/main/java/br/com/quatrodcum/myhealth/view/register/RegisterActivity.kt
@@ -3,6 +3,7 @@ package br.com.quatrodcum.myhealth.view.register
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import br.com.quatrodcum.myhealth.databinding.ActivityRegisterBinding
 
@@ -15,6 +16,20 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        val items = arrayOf(
+            "1",
+            "2"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.cbxObjective.adapter = adapter
     }
 
     companion object {
