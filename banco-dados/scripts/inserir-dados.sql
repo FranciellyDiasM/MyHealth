7310cd9883dacab48df8e9fff3309275cf906550
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2024-10-03 18:24:40 -0300

    inserts das tabelas unidade_medida e refeicao

--- banco-dados/scripts/inserir-dados.sql
+++ banco-dados/scripts/inserir-dados.sql
@@ -1,4 +1,9 @@
 INSERT INTO objetivo (descricao) VALUES ('Ganhar massa'),  ('Perder peso');
 
-INSERT INTO usuario 	(nome, 	email,  senha, 	imc, objetivo_id)  VALUES  ('joao',  'joao@email.com', '123456',  22.7,	 1);
+INSERT INTO usuario (nome, 	email,  senha, 	imc, objetivo_id)  VALUES  ('joao',  'joao@email.com', '123456',  22.7,	 1);
 INSERT INTO usuario  (nome,  email,  senha,  imc, objetivo_id)  VALUES  ('maria',  'maria@email.com', '654321',  27.7,	 2);
+
+INSERT INTO unidade_medida  (nome)  VALUES  ('grama'), ('pitada');
+
+INSERT INTO refeicao  (nome,  descricao,  calorias,  modo_preparo, objetivo_id, usuario_id)  VALUES  ('ovo frito',  'ovo com sal direto na frigideira',  107.0,	'Ligar fogão, colocar óleo em uma frigideira e levá-la ao fogo, adicionar o ovo e o sal, esperar fritar e virar para fritar o outro lado, e fim.', 1, 1);
+
