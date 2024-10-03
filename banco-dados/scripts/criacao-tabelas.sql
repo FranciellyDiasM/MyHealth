860db2dfbf2a2e8f98daab9f1fa28b296fe3fc6f
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2024-10-03 18:10:22 -0300

    criação de tabelas e alguns relacionamentos

--- banco-dados/scripts/criacao-tabelas.sql
+++ banco-dados/scripts/criacao-tabelas.sql
@@ -12,3 +12,32 @@ CREATE TABLE usuario (
 	objetivo_id INTEGER NOT NULL,
 	FOREIGN KEY (objetivo_id) REFERENCES objetivo(id)
 );
+
+CREATE TABLE ingrediente (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	nome TEXT NOT NULL
+);
+
+CREATE TABLE refeicao (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	nome TEXT NOT NULL,
+	descricao TEXT NOT NULL,
+	calorias REAL NOT NULL,
+	modo_preparo TEXT NOT NULL,
+	objetivo_id INTEGER NOT NULL,
+	FOREIGN KEY (objetivo_id) REFERENCES objetivo(id),
+	FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+
+CREATE TABLE unidade_medida (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	nome TEXT NOT NULL
+);
+
+CREATE TABLE ingrediente_refeicao (
+	FOREIGN KEY (ingrediente_id) REFERENCES ingrediente(id),
+	FOREIGN KEY (refeicao_id) REFERENCES refeicao(id),
+	quantidade REAL NOT NULL,
+	FOREIGN KEY (unidade_medida_id) REFERENCES unidade_medida(id)
+);
+
