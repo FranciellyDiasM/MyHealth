9a0392aebfb53372904e3f137197aa796bb09e79
juant-tavares <139511597+juant-tavares@users.noreply.github.com>
2024-10-22 17:04:36 -0300

    Delete em cascata
    Trouxe o delete das chaves estrangeiras em cascata.

--- banco-dados/scripts/criacao-tabelas.sql
+++ banco-dados/scripts/criacao-tabelas.sql
@@ -7,10 +7,15 @@ CREATE TABLE usuario (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
-    senha TEXT NOT NULL,
     imc REAL NOT NULL,
+    senha TEXT NOT NULL,
     objetivo_id INTEGER NOT NULL,
-    FOREIGN KEY (objetivo_id) REFERENCES objetivo(id)
+    FOREIGN KEY (objetivo_id) REFERENCES objetivo(id) ON DELETE CASCADE
+);
+
+CREATE TABLE unidade_medida (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL
 );
 
 CREATE TABLE ingrediente (
@@ -25,23 +30,15 @@ CREATE TABLE refeicao (
     calorias REAL NOT NULL,
     modo_preparo TEXT NOT NULL,
     objetivo_id INTEGER NOT NULL,
-    usuario_id INTEGER NOT NULL, -- Adicionando a coluna usuario_id
-    FOREIGN KEY (objetivo_id) REFERENCES objetivo(id),
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id) -- Agora pode referenciar
-);
-
-CREATE TABLE unidade_medida (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL
+    FOREIGN KEY (objetivo_id) REFERENCES objetivo(id) ON DELETE CASCADE
 );
 
 CREATE TABLE ingrediente_refeicao (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Adicionei uma chave primária
     ingrediente_id INTEGER NOT NULL,
     refeicao_id INTEGER NOT NULL,
     quantidade REAL NOT NULL,
     unidade_medida_id INTEGER NOT NULL,
-    FOREIGN KEY (ingrediente_id) REFERENCES ingrediente(id),
-    FOREIGN KEY (refeicao_id) REFERENCES refeicao(id),
-    FOREIGN KEY (unidade_medida_id) REFERENCES unidade_medida(id)
+    FOREIGN KEY (ingrediente_id) REFERENCES ingrediente(id) ON DELETE CASCADE,
+    FOREIGN KEY (refeicao_id) REFERENCES refeicao(id) ON DELETE CASCADE,
+    FOREIGN KEY (unidade_medida_id) REFERENCES unidade_medida(id) ON DELETE CASCADE
 );
